176f0101febde0551b62ec40de7fe739fc7dadeb
Samantha <samantha@paddle-booking.dev>
2026-03-05 08:41:00 +0100

    Use adapter-auto with Vercel config for Node 24 deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,9 +6,7 @@ const config = {
     dev: process.env.NODE_ENV === 'development'
   },
   kit: {
-    adapter: adapter({
-      runtime: 'nodejs18.x'
-    }),
+    adapter: adapter(),
   },
 };
 
